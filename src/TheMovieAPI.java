9e9efb6b04c7b67d1e3ba656c30510d6110a69ae
IagoBoebel <114117586+IagoBoebel@users.noreply.github.com>
2025-02-28 17:48:57 -0300

    Update TheMovieAPI.java

--- src/TheMovieAPI.java
+++ src/TheMovieAPI.java
@@ -7,7 +7,7 @@ import java.net.http.HttpResponse;
 public class TheMovieAPI implements APIClient {
 
     private URI uri = URI.create("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&region=Brazil");
-    private String apiToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNWI1ODQyN2QxMGI3MmEwNzFmNmQyYmZiMDA1NzlmYSIsIm5iZiI6MTczOTk4MDI2OC41ODYsInN1YiI6IjY3YjVmZGVjZDI4ZTg3ZTBlNWUzZGI5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2lXkfPClxo9Nx3kNlvxLLCgiuIVjnP0wnSu34LFQV9I";
+    private String apiToken = "<Seu apiToken>";
     private HttpClient client;
 
     public TheMovieAPI() {
